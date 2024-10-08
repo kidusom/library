c24bb384d3919b763790369a7d6c9bcdc58d07b9
obi <obi@obi.obi>
2024-10-08 02:08:42 -0400

    1

---
+++ api/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Book model in the library
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)   
+    published_date = models.DateField()
+    copies_available = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+
+# UserProfile model to extend User model with additional information
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_membership = models.DateField(auto_now_add=True)
+    active_status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
+
+#  create a UserProfile whenever a new User is created
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+# Transaction model that tracks borrowing and returning of books
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    checked_out_date = models.DateTimeField(auto_now_add=True)
+    returned_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
